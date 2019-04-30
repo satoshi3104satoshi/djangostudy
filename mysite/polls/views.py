4493290edb2925a773391952850d4c87b794cf58
satoshi3104satoshi <3104.ktym@gmail.com>
2019-05-01 02:06:36 +0900

    Second time

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Question
-from django.http import HttpResponse Http404
+from django.http import HttpResponse, Http404
 
 
 def index(request):
@@ -8,8 +8,12 @@ def index(request):
     context = {'latest_question_list': latest_question_list,}
     return render(request, 'polls/index.html',context)
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html',{'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."

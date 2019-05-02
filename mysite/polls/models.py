589815367482122432e84a6c2ebf3d489cf4ddea
satoshi3104satoshi <3104.ktym@gmail.com>
2019-05-03 01:02:20 +0900

    error now in models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,6 +10,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
+        now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
